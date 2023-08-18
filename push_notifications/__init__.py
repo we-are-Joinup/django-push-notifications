71d954001ada716aeb26f4dab155467dc6152a4e
Pablo Martín <goinnn@gmail.com>
2023-08-18 13:35:27 +0200

    Fix packagename

--- push_notifications/__init__.py
+++ push_notifications/__init__.py
@@ -1,7 +1,7 @@
 import pkg_resources
 
 
-__version__ = pkg_resources.require("django-push-notifications-rubgomez93")[0].version
+__version__ = pkg_resources.require("django-push-notifications-joinup")[0].version
 
 
 class NotificationError(Exception):
